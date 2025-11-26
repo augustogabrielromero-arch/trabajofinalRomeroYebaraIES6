22461cd39410513bc8a2d825bee5282f25a8cc8a
unknown <augustogabrielromero-arch>
2025-11-26 01:53:34 -0300

    Inicialización TrabajoFinalRomero IES6

---
+++ src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //Java es fuertemente tipado
+        System.out.println("Hello, IES Nº6 Informática!");
+    }
+}
